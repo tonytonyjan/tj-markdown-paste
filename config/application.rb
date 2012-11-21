a4c530048e6c8e54eaa353ce0f16095b783a6321
Tony Jian <tonytonyjan@gmail.com>
2012-11-21 13:13:35 +0800

    fix precompile problem

--- config/application.rb
+++ config/application.rb
@@ -62,5 +62,8 @@ module MarkupPaste
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Using devise on Heroku with Ruby on Rails 3.1 requires setting:
+    config.assets.initialize_on_precompile = false
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,4 @@ MarkupPaste::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  # Using devise on Heroku with Ruby on Rails 3.1 requires setting:
-  config.assets.initialize_on_precompile = false
 end
