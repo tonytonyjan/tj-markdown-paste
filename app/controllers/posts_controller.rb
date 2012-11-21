661dd3ca7242d6d30d8caae8a752f60f5871b7c6
Tony Jian <tonytonyjan@gmail.com>
2012-11-21 12:01:14 +0800

    add meta

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,7 @@ class PostsController < ApplicationController
   load_and_authorize_resource
   default_resource_actions
   def show
+    @meta = {description: @post.content}
     render layout: "clean"
   end
 end
