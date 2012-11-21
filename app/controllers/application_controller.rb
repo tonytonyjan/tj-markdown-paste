97ed9c786195b0da8e026f11698f9f4cb141a479
Tony Jian <tonytonyjan@gmail.com>
2012-11-21 08:13:01 +0800

    add ability

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to request.referer || root_url, :alert => exception.message
+  end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  load_and_authorize_resource
   default_resource_actions
   def show
     render layout: "clean"
