1e1f7fdcd90571b7d8722034bf98dac3ccbb035f
Tony Jian <tonytonyjan@gmail.com>
2012-11-23 17:37:26 +0800

    support gfm

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ActiveRecord::Base
   attr_accessible :content, :title, :show_title
   validates :title, :content, presence: true
   def markup_content
-    RDiscount.new(content, :strict).to_html.html_safe
+    RDiscount.new(GFM::gfm(content), :strict).to_html.html_safe
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,9 @@ end
 
 module MarkupPaste
   class Application < Rails::Application
-            config.generators do |g|
-              g.stylesheets false
-            end
+    config.generators do |g|
+      g.stylesheets false
+    end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
@@ -21,6 +21,7 @@ module MarkupPaste
 
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
+    config.autoload_paths += %W(#{config.root}/lib)
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
