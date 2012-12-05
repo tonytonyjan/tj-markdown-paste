e92200c1cab53e55a8ebcaa98b0008a75089b2cd
Tony Jian <tonytonyjan@gmail.com>
2012-12-06 05:19:42 +0800

    split RSpec settings for Devise into a file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -35,8 +35,4 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
-
-  RSpec.configure do |config|
-    config.include Devise::TestHelpers, :type => :controller
-  end
 end
